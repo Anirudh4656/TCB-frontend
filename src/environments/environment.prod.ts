1317259a9e525c5ee7e57f36b3c6aaf7831b7a67
ANIRUDH <anirudh4656@gmail.com>
2023-06-19 17:27:10 +0530

    f

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,15 @@
+// export const environment = {
+//   production: true,
+//   ADMIN_API_URL: 'https://tcbback.onrender.com/api/admin',
+//   PUBLIC_API_URL: 'https://tcbback.onrender.com/api/public'
+// };
+export const environment = {
+  production: true,
+  ADMIN_API_URL: 'https://tcb-production-be2c.up.railway.app/api/admin',
+  PUBLIC_API_URL: 'https://tcb-production-be2c.up.railway.app/api/public'
+};
+// export const environment = {
+//   production: true,
+//   ADMIN_API_URL: 'http://localhost:3000/api/admin',
+//   PUBLIC_API_URL: 'http://localhost:3000/api/public'
+// };
