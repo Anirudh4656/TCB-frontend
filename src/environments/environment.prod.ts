86748f617aac47456a77c0c90ce6e84b85ff31ac
ANIRUDH <anirudh4656@gmail.com>
2023-06-19 17:57:26 +0530

    f

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,8 +5,8 @@
 // };
 export const environment = {
   production: true,
-  ADMIN_API_URL: 'https://tcb-production-be2c.up.railway.app/api/admin',
-  PUBLIC_API_URL: 'https://tcb-production-be2c.up.railway.app/api/public'
+  ADMIN_API_URL: 'http://www.api.thechoicebox.com/admin',
+  PUBLIC_API_URL: 'http://www.api.thechoicebox.com/public'
 };
 // export const environment = {
 //   production: true,
